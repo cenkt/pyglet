5b351a285438809cd2e97e5641dd0f80f8df7691
cenkt <cenktulumen@hotmail.com>
2022-03-05 18:41:56 +0300

    feat:

--- myprojects/5-n-body apply gravity.py
+++ myprojects/5-n-body apply gravity.py
@@ -4,13 +4,13 @@ import random
 import math
 
 
-win_x = 910
-win_y = 900
-num_objects = 5
+win_x = 400
+win_y = 400
+num_objects = 1
 speed_range = 5
 speed_mag_limit = 50
-acc_range = 0
-acc_mag_limit = 0.1
+# acc_range = 0
+# acc_mag_limit = 0.1
 lim_mass = 300
 
 
@@ -50,7 +50,7 @@ class Space(pyglet.window.Window):
     def __init__(self, width, height, noo):
         super().__init__(width, height)
         self.number_of_obj = noo
-        self.gravity = Vector(0.001, -0.9)
+        self.gravity = Vector(0.1, -0.2)
         self.batch = pyglet.shapes.Batch()
         self.list_of_obj = []
         self.mouse = Vector(0, 0)
@@ -62,8 +62,8 @@ class Space(pyglet.window.Window):
     #     self.mouse = Vector(x, y)
 
     def create_planet(self):
-        ox, oy = random.uniform(5, self.width), 500
-        omass = random.uniform(5, lim_mass)
+        ox, oy = 200, 200
+        omass = 256
         odx, ody = 0, 0
         oddx, oddy = 0, 0
         ocolor = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
@@ -112,25 +112,28 @@ class Planet(pyglet.shapes.Circle, Vector):
         self.acc = vec
 
     def check_edge(self):
-        if (self.y - self.radius) < 0:
-            self.y = 0
+        if (self.y) <= self.radius:
+            self.y = self.radius
             self.speed.y = -self.speed.y
-        if (self.y + self.radius) > space.height:
-            self.y = space.height
+
+        if (self.y) >= space.height - self.radius:
+            self.y = space.height - self.radius
             self.speed.y = -self.speed.y
-        if (self.x - self.radius) < 0:
-            self.x = 0
+
+        if (self.x) <= self.radius:
+            self.x = self.radius
             self.speed.x = -self.speed.x
-        if (self.x + self.radius) > space.width:
-            self.x = space.width
+
+        if (self.x) >= space.width - self.radius:
+            self.x = space.width - self.radius
             self.speed.x = -self.speed.x
 
     def move(self, dt):
         old_x, old_y = self.x, self.y
         self.calculate_acc(space.gravity)
         self.speed.plus_v(self.acc)
-        self.check_edge()
         self.plus_v(self.speed)
+        self.check_edge()
         trail = pyglet.shapes.Line(self.x, self.y, old_x, old_y, 1, batch=space.batch)
         trail.opacity = 100
         self.trails.append(trail)
