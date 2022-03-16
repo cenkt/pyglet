06a069c6d0520036850f24089070d589bc7f2c35
cenkt <cenktulumen@hotmail.com>
2022-03-16 11:30:33 +0300

    feat: OLS implementation

--- myprojects/LeastSquareError.py
+++ myprojects/LeastSquareError.py
@@ -47,9 +47,10 @@ class Canvas(pyglet.window.Window):
         self.batch = pyglet.shapes.Batch()
         self.list_of_obj = []
         self.mouse = Vector(0, 0)
+        self.line = self.line = pyglet.shapes.Line(0, 0, 0, 0, width=0, color=(0, 0, 0))
 
     def on_mouse_press(self, x, y, button, modifiers):
-        return
+        self.list_of_obj.append(Dot(x, y, 3, 0, 0, 0, 0, (100, 100, 100), self.batch))
 
     def on_mouse_release(self, x, y, button, modifiers):
         return
@@ -57,13 +58,31 @@ class Canvas(pyglet.window.Window):
     def create_planet(self):
         return
 
+    def calculate_line(self, loo):
+        x_avg, y_avg = 0, 0
+        for i in loo:
+            x_avg += i.x
+            y_avg += i.y
+        x_avg = x_avg / len(loo)
+        y_avg = y_avg / len(loo)
+        num, den = 0, 0
+        for i in loo:
+            num += (i.x - x_avg) * (i.y - y_avg)
+            den += (i.x - x_avg) * (i.x - x_avg)
+        return num / den, y_avg - (x_avg * num / den)
+
     def update(self, dt):
-        for obj in self.list_of_obj:
-            obj.move(dt)
+        if len(self.list_of_obj) > 1:
+            m, b = self.calculate_line(self.list_of_obj)
+            x1, x2 = 0, self.width
+            y1, y2 = x1 * m + b, x2 * m + b
+            self.line = pyglet.shapes.Line(x1, y1, x2, y2, width=1, color=(250, 200, 100))
+        return
 
     def on_draw(self):
         self.clear()
         self.batch.draw()
+        self.line.draw()
 
 
 class Dot(pyglet.shapes.Circle, Vector):
